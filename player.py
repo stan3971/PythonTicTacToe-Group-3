02fd9fbf04cc1dc1e2efc199a89cd8c62e8d7fe6
Bryan Stander <stan3971@maggie.cs.pacificu.edu>
2014-10-24 12:40:02 -0700

    player.py

---
+++ player.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+##########################################
+# File Name:player.py
+# Author:Group 3
+# Date:10/24/14
+# Class:360
+# Assignment:Tic-Tac-Toe
+# Purpose:Player Superclass. holds user/ai
+########################################
